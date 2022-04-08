857d6b948212602c2394be11332585210a24ae68
Alessio Napoli <alessionapoli99@icloud.com>
2022-04-08 14:06:30 +0200

    Added table Tags and added Relationship between Posts and Tags

--- app/Post.php
+++ app/Post.php
@@ -6,9 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'url', 'description', 'slug', 'category_id'];
+    protected $fillable = ['title', 'url', 'description', 'slug', 'category_id', 'tag'];
 
     public function category(){
         return $this->belongsTo('App\Category');
+    }
+
+    public function tag(){
+        return $this->belongsToMany('App\Tag');
     }
 }
